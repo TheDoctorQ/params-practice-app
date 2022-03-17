a4d1cfd3b9a1170bb96ecced5165593f33eb00fd
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-16 22:24:08 -0600

    adds methods & routes

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,15 @@
 class ParamsController < ApplicationController
+
+  def caps
+    render json: {message: "#{params[:user_input].upcase}"
+  end
+
+  def segment
+    render json: {message: "#{params[:wildcard].upcase}"
+  end 
+
+  def body  
+    render json: {message: "purple hippo"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/caps" => "params#caps"
+
+  post "/caps" => "params#body"
+
+  get "/caps/:wildcard" => "params#segment" 
+
 end
